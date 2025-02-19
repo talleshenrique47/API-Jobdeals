b6d148cb493ef7e13586dc67ea1a00afde9b747f
Talles Henrique <81825848+talleshenrique47@users.noreply.github.com>
2025-02-19 20:46:27 -0300

    Update Settings.cs

--- JobDealsAPI/Settings.cs
+++ JobDealsAPI/Settings.cs
@@ -2,6 +2,6 @@
 {
     public static class Settings
     {
-        public static string Secret = "8e14021162be4a185c6fc53bc4d694e2";
+        public static string Secret = "Trocar a senha";
     }
 }
